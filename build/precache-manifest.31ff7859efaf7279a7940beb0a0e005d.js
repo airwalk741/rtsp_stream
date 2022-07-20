518ab0e12b79db9f113f168c20fc83a642314226
airwalk741 <airwalk741@airwalk741s-Mac-mini.local>
2022-07-20 18:28:08 +0900

    init

---
+++ build/precache-manifest.31ff7859efaf7279a7940beb0a0e005d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "579106d44f4300a9970a",
+    "url": "/static/css/main.196274e4.chunk.css"
+  },
+  {
+    "revision": "579106d44f4300a9970a",
+    "url": "/static/js/main.579106d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "320b6a83555a0686cf17",
+    "url": "/static/js/2.320b6a83.chunk.js"
+  },
+  {
+    "revision": "7966320869a2d78df9ef36821bd9a2e6",
+    "url": "/index.html"
+  }
+];
