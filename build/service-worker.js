c26767f2c787cdbc54da6b46e6907e9e86bd2648
airwalk741 <airwalk741@airwalk741s-Mac-mini.local>
2022-07-21 12:31:40 +0900

    build

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.47bebc8621de010dc7ba541adbdcda47.js"
+  "./precache-manifest.a38848c2966cd841481f24a6c0969d49.js"
 );
 
 workbox.clientsClaim();
